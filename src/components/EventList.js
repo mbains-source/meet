23914feba19db5654645625a763fc505d144d064
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-08-07 22:35:53 -0700

    react testing library mindset

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,9 +1,15 @@
 // src/components/EventList.js
 
-const EventList = () => {
-  return (
-    <ul id="event-list"></ul>
-  );
+import Event from "./Event";
+
+const EventList = ({ events }) => {
+ return (
+   <ul id="event-list">
+     {events ?
+       events.map(event => <Event key={event.id} event={event} />) :
+       null}
+   </ul>
+ );
 }
 
 export default EventList;
