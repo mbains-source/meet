977103cd74977a38cd87bfd6cfd1f3670b3d9c53
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-08-07 22:22:21 -0700

    create eventlist.js and update app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // src/App.js
 
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
- return (
-   <div className="App">
-     <div id="event-list"></div>
-   </div>
- );
+  return (
+    <div className="App">
+      <EventList />
+    </div>
+  );
 }
 
 export default App;
