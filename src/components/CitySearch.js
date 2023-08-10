0b79c29d81657395b5caeb61db1aec79e44bbf65
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-08-09 23:38:19 -0700

    create api.js and uppdates in order to pass test

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -2,19 +2,49 @@
 
 import { useState } from "react";
 
-const CitySearch = () => {
+const CitySearch = ({ allLocations }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
-  return (
-    <div id="city-search">
-      <input
-        type="text"
-        className="city"
-                placeholder="Search for a city"
-        onFocus={() => setShowSuggestions(true)}
-      />
-      {showSuggestions ? <ul className="suggestions"></ul> : null}
-    </div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-export default CitySearch;
+  <input
+  type="text"
+  className="city"
+          placeholder="Search for a city"
+  value={query}
+  onFocus={() => setShowSuggestions(true)}
+/>
+
+const handleInputChanged = (event) => {
+  const value = event.target.value;
+  const filteredLocations = allLocations ? allLocations.filter((location) => {
+    return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+  }) : [];
+
+  setQuery(value);
+  setSuggestions(filteredLocations);
+};
+
+<div id="city-search">
+<input
+  type="text"
+  className="city"
+          placeholder="Search for a city"
+  value={query}
+  onFocus={() => setShowSuggestions(true)}
+  onChange={handleInputChanged}
+/>
+{showSuggestions ? <ul className="suggestions"></ul> : null}
+</div>
+
+{showSuggestions ?
+  <ul className="suggestions">
+    {suggestions.map((suggestion) => {
+      return <li key={suggestion}>{suggestion}</li>
+    })}
+    <li key='See all cities'>
+      <b>See all cities</b>
+    </li>
+  </ul>
+  : null
+}
