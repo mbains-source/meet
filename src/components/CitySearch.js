f462f041c9b1bc04fca005d7d58b34044a662a84
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-08-09 23:48:30 -0700

    part 3 final before task

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -25,26 +25,33 @@ const handleInputChanged = (event) => {
   setSuggestions(filteredLocations);
 };
 
-<div id="city-search">
-<input
-  type="text"
-  className="city"
-          placeholder="Search for a city"
-  value={query}
-  onFocus={() => setShowSuggestions(true)}
-  onChange={handleInputChanged}
-/>
-{showSuggestions ? <ul className="suggestions"></ul> : null}
-</div>
+const handleItemClicked = (event) => {
+  const value = event.target.textContent;
+  setQuery(value);
+  setShowSuggestions(false); // to hide the list
+};
 
-{showSuggestions ?
-  <ul className="suggestions">
-    {suggestions.map((suggestion) => {
-      return <li key={suggestion}>{suggestion}</li>
-    })}
-    <li key='See all cities'>
-      <b>See all cities</b>
-    </li>
-  </ul>
-  : null
+return (
+  <div id="city-search">
+    <input
+      type="text"
+      className="city"
+      placeholder="Search for a city"
+      value={query}
+      onFocus={() => setShowSuggestions(true)}
+      onChange={handleInputChanged}
+    />
+    {showSuggestions ?
+      <ul className="suggestions">
+        {suggestions.map((suggestion) => {
+          return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+        })}
+        <li key='See all cities' onClick={handleItemClicked}>
+          <b>See all cities</b>
+        </li>
+      </ul>
+      : null
+    }
+  </div>
+)
 }
