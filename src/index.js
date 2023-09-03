4b434c2c6b15737a28bdcfe8594c809823e40680
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-02 19:30:01 -0700

    removing atatus code from index ahead of node 16

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import * as atatus from 'atatus-spa';
-atatus.config('d7f413e897dc4406ba41221b47b56eeb').install();
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
