1245eb9fd4f7c5ed677f5204dfad991af6bd1640
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-10 21:20:41 -0700

    Add InfoAlert and ErrorAlert to app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-// src/App.js
-
-import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
-import { useEffect, useState } from 'react';
-import { extractLocations, getEvents } from './api';
-
 import './App.css';
+import { useState, useEffect } from 'react';
+import { extractLocations, getEvents } from './api';
+import { ErrorAlert, InfoAlert } from './components/Alert';
 
 const App = () => {
-  const [allLocations, setAllLocations] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
-  const [currentCity, setCurrentCity] = useState("See all cities");
-
-  useEffect(() => {
-    fetchData();
-  }, [currentCity]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState('See all cities');
+  const [infoAlert, setInfoAlert] = useState('');
+  const [errorAlert, setErrorAlert] = useState('');
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
+    const filteredEvents =
+      currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
-  }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity, currentNOE]);
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents />
-      <EventList events={events} />
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+      </div>
+      <NumberOfEvents
+        setCurrentNOE={setCurrentNOE}
+        setErrorAlert={setErrorAlert}
+      ></NumberOfEvents>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      ></CitySearch>
+      <EventList events={events}></EventList>
     </div>
   );
-}
+};
 
 export default App;
