8b4278d66aee070056fe137b0effb04dc01adfc1
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-12 23:08:36 -0700

    update alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,3 @@
-// src/components/Alert.js
-
 import { Component } from 'react';
 
 class Alert extends Component {
@@ -9,29 +7,21 @@ class Alert extends Component {
     this.bgColor = null;
   }
 
-  class InfoAlert extends Alert {
-    constructor(props) {
-      super(props);
-      this.color = 'rgb(0, 0, 255)'; // blue
-      this.bgColor = 'rgb(220, 220, 255)'; // light blue
-    }
-  }
-
   getStyle = () => {
     return {
       color: this.color,
       backgroundColor: this.bgColor,
-      borderWidth: "2px",
-      borderStyle: "solid",
-      fontWeight: "bolder",
-      borderRadius: "7px",
+      borderWidth: '2px',
+      borderStyle: 'solid',
+      fontWeight: 'bolder',
+      borderRadius: '7px',
       borderColor: this.color,
-      textAlign: "center",
-      fontSize: "12px",
-      margin: "10px 0",
-      padding: "10px"
+      textAlign: 'center',
+      fontSize: '12px',
+      margin: '10px 0',
+      padding: '10px',
     };
-  }
+  };
 
   render() {
     return (
@@ -41,3 +31,29 @@ class Alert extends Component {
     );
   }
 }
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(0, 0, 255)'; // blue
+    this.bgColor = 'rgb(220, 220, 255)'; // light blue
+  }
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 0, 0)'; // red
+    this.bgColor = 'rgb(255, 200, 200)'; // light red
+  }
+}
+
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 99, 77)'; // orange
+    this.bgColor = 'rgb(255, 200, 200)'; // light orange
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
