fe35e288a5f1ff728f5bce3cc4bd6f5ea07c811e
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-17 22:07:50 -0700

    revised api

--- src/api.js
+++ src/api.js
@@ -1,86 +1,57 @@
-import mockData from './mock-data';
+import { mockData } from "./mock-data";
+import axios from "axios";
+import NProgress from "nprogress";
 
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
-export const extractLocations = (events) => {
-  const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
-  return locations;
-};
-
-const checkToken = async (accessToken) => {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  );
-  const result = await response.json();
-  return result;
-};
-
-const removeQuery = () => {
-  let newurl;
-  if (window.history.pushState && window.location.pathname) {
-    newurl =
-      window.location.protocol +
-      '//' +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState('', '', newurl);
-  } else {
-    newurl = window.location.protocol + '//' + window.location.host;
-    window.history.pushState('', '', newurl);
-  }
-};
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem('access_token', access_token);
-
-  return access_token;
-};
 
 export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
+  const accessToken = localStorage.getItem("access_token");
   const tokenCheck = accessToken && (await checkToken(accessToken));
   if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem('access_token');
+    await localStorage.removeItem("access_token");
     const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get('code');
+    const code = await searchParams.get("code");
     if (!code) {
-      const response = await fetch(
-        'https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url'
+      const results = await axios.get(
+        "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url"
       );
-      const result = await response.json();
-      const { authUrl } = result;
+      const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
     return code && getToken(code);
   }
-
   return accessToken;
 };
 
-/**
- *
- * This function will fetch the list of all events
- */
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
 export const getEvents = async () => {
-  if (window.location.href.startsWith('http://localhost')) {
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
     return mockData;
   }
+  //Access local storage when user offline 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    console.log('offline data: ', data)
+    return data ? JSON.parse(data).events : [];
+
+}
 
   if (!navigator.onLine) {
-    const events = localStorage.getItem('lastEvents');
-    return events ? JSON.parse(events) : [];
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(data).events:[];;
   }
 
   const token = await getAccessToken();
@@ -88,14 +59,50 @@ export const getEvents = async () => {
   if (token) {
     removeQuery();
     const url =
-      'https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-events' + '/' + token;
-      
-      const response = await fetch(url);
-      const result = await response.json();
-      if (result) {
-        localStorage.setItem('lastEvents', JSON.stringify(result.events));
-        return result.events;
-      } else return null;
-    
+      "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
   }
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+      "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/token" +  "/" + encodeCode);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    error.json();
+  }
+};
+
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
 };
