942658021ebd6ff5f725ac80322bcd867bf8947d
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-18 22:52:16 -0700

    install recharts + cityeventschart

--- src/App.js
+++ src/App.js
@@ -37,23 +37,22 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
-        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
         {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
       </div>
-      <NumberOfEvents
-        setCurrentNOE={setCurrentNOE}
-        setErrorAlert={setErrorAlert}
-      ></NumberOfEvents>
       <CitySearch
         allLocations={allLocations}
         setCurrentCity={setCurrentCity}
-        setInfoAlert={setInfoAlert}
-      ></CitySearch>
-      <EventList events={events}></EventList>
+        setInfoAlert={setInfoAlert} />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <CityEventsChart allLocations={allLocations} events={events} />
+      <EventList events={events} />
     </div>
-  );
+ );
+ 
 };
 
 export default App;
---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,16 @@
+// src/components/CityEventsChart.js
+
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis, YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
+
+const CityEventsChart = () => {
+}
+
+export default CityEventsChart;
