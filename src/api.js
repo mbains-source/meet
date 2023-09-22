5f3af16e09ebeb88c1b34997bceb774cce07cd55
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-21 23:33:14 -0700

    api.js

--- src/api.js
+++ src/api.js
@@ -1,108 +1,101 @@
-import { mockData } from "./mock-data";
+import mockData from './mock-data';
 import axios from "axios";
 import NProgress from "nprogress";
 
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
-
-const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-
-  return result;
-};
-
-export const getEvents = async () => {
-  NProgress.start();
-
-  if (window.location.href.startsWith("http://localhost")) {
-    NProgress.done();
-    return mockData;
-  }
-  //Access local storage when user offline 
-  if (!navigator.onLine) {
-    const data = localStorage.getItem("lastEvents");
-    NProgress.done();
-    console.log('offline data: ', data)
-    return data ? JSON.parse(data).events : [];
-
+// takes an event array, uses map to create new array with only locations
+// remove all duplicates by creating another new array using spread operator
+// set remove all duplicates from array
+export const extractLocations = (events) => {
+    const extractedLocations = events.map((event) => event.location)
+    const locations = [...new Set(extractedLocations)];
+    return locations;
 }
 
-  if (!navigator.onLine) {
-    const data = localStorage.getItem("lastEvents");
-    NProgress.done();
-    return data?JSON.parse(data).events:[];;
-  }
+// function check token's validity
+const checkToken = async (accessToken) => {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+    const result = await response.json();
+    return result;
+  };
 
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =
-      "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-events" + "/" + token;
-    const result = await axios.get(url);
-    if (result.data) {
-      var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
-    }
-    NProgress.done();
-    return result.data.events;
-  }
-};
-
+// remove code from URL
 const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+      newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
+    } else {
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
+    }
+  };
 
+// get new token - redirect user login with Google, redirect user to site with code
 const getToken = async (code) => {
-  try {
     const encodeCode = encodeURIComponent(code);
     const response = await fetch(
-      "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/token" +  "/" + encodeCode);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+      'https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    );
     const { access_token } = await response.json();
     access_token && localStorage.setItem("access_token", access_token);
+  
     return access_token;
-  } catch (error) {
-    error.json();
-  }
-};
+  };
 
-export const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  var locations = [...new Set(extractLocations)];
-  return locations;
-};
+
+// fetch list of all events
+export const getEvents = async () => {
+
+    if (window.location.href.startsWith("http://localhost")) {
+      return mockData;
+    }
+  
+    if (!navigator.onLine) {
+        const events = localStorage.getItem('lastEvents');
+        return events ? JSON.parse(events) : [];
+    }
+
+    const token = await getAccessToken();
+  
+    if (token) {
+      removeQuery();
+      const url =  "https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-events" + "/" + token;
+      const response = await fetch(url);
+      const result = await response.json();
+      if (result) {
+        localStorage.setItem("lastEvents", JSON.stringify(result.events));
+        return result.events;
+      } else return null; 
+    }
+  };
+
+//get access token
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token')
+
+    // checks for access token
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    // if no token found, code checks for authorisation code
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        
+        //
+        if (!code) {
+            const response = await fetch('https://sud9585dl4.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url');
+            const result = await response.json();
+            const { authUrl } = result;
+            return (window.location.href = authUrl)
+        }
+        return code && getToken(code)
+    }
+    return accessToken;
+}
