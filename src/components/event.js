87cba61c352935d0bc64baeefdbae89e42e4fb14
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-09-27 13:03:40 -0700

    Update event.js

--- src/components/event.js
+++ src/components/event.js
@@ -1,44 +1,30 @@
-// src/components/Event.js
+import { useState } from 'react';
 
-import React, { Component } from "react";
-
-class Event extends Component {
-  state = { collapsed: true };
-  toggleDetails = () => {
-    this.setState((prevState) => ({
-      collapsed: !prevState.collapsed,
-    }));
-  };
-
-  render() {
-    const { event } = this.props;
-    const { collapsed } = this.state;
-
+const Event = ({ event }) => {
+   const [showDetails, setShowDetails] = useState(false);
+   
     return (
-      <div className={`Event ${this.state.collapsed ? "" : "expanded"}`}>
-        <h2 className="summary">{event.summary}</h2>
-        <p className="start">{event.start.dateTime}</p>
-        <p className="location">{`Location: ${event.location}`}</p>
-        <button className="details-button" onClick={this.toggleDetails}>
-          {collapsed ? "show" : "hide"} details
-        </button>
-        {!collapsed && (
-          <div className="details">
-            <h3 className="about">About this event:</h3>
-            <a
-              className="link"
-              href={event.htmlLink}
-              target="_blank"
-              rel="noopener noreferrer"
+        <li className="event">
+            <h3>{event.summary}</h3>
+            <p>{event.location}</p>
+            <p>{new Date(event.created).toUTCString()}</p>
+            <button className="details-btn"
+            onClick = { () => {
+                setShowDetails(!showDetails);
+            }}
             >
-              See details on Google Calendar
-            </a>
-            <p className="description">{event.description}</p>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+                {showDetails ? 'Hide Details' : 'Show Details'}
+            </button>
+
+            {showDetails ? (
+                <div className="details">
+                    <h4> Event Details </h4>
+                    <p> Description: {event.description}</p>
+                    <p>Event status: {event.status}</p>
+                </div>
+            ) : null}
+        </li>
+    )
+};
 
 export default Event;
